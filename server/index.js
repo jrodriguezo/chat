189828d1e87e588d1609606207a89fa32af918e7
jrodriguezo <jrodriguezo@alumnos.upm.es>
2024-10-06 02:16:15 +0200

    feat: use turso database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import logger from "morgan";
 
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import { createClient } from "@libsql/client";
+
+dotenv.config();
 
 const port = process.env.PORT ?? 3000;
 
@@ -12,16 +16,54 @@ const io = new Server(server, {
   connectionStateRecovery: {},
 });
 
-io.on("connection", (socket) => {
+const db = createClient({
+  url: process.env.DB_URL,
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT
+    )
+  `);
+
+io.on("connection", async (socket) => {
   console.log("An user has connected");
 
   socket.on("disconnect", () => {
     console.log("an user has disconnected");
   });
 
-  socket.on("chat message", (message) => {
-    io.emit("chat message", message);
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.execute({
+        sql: "INSERT INTO messages (content) VALUES (:msg)",
+        args: { msg },
+      });
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+
+    io.emit("chat message", msg, result.lastInsertRowid.toString());
   });
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: "SELECT id, content FROM messages WHERE id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0],
+      });
+
+      results.rows.forEach((row) => {
+        socket.emit("chat message", row.content, row.id.toString());
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 });
 
 app.use(logger("dev"));
